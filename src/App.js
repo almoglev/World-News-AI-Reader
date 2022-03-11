49f43b4b74d28cc07ed938c4d7bfbdaa97058479
Almog Lev <almoglev3@gmail.com>
2022-03-11 16:18:11 +0200

    Set up AlanAI + fetching articles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import alanBtn from "@alan-ai/alan-sdk-web"
+
+const alanKey = 'f1a329f1a7aa78f8608827a4e65848d12e956eca572e1d8b807a3e2338fdd0dc/stage'
 
 function App() {
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles }) => {
+        if(command === 'newHeadlines') {
+          console.log(articles)
+        }
+      }
+    })
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Alan AI News Application</h1>
     </div>
   );
 }
